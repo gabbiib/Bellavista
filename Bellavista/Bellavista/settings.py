c568f80f31cbd150c080c7e64320fb28ebe445dd
Gabriel Castillo <gabrielolicas66@gmail.com>
2024-11-04 16:35:57 -0300

    HOST de Heroku añadido

--- Bellavista/Bellavista/settings.py
+++ Bellavista/Bellavista/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'canalbellavista.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'canalbellavista.herokuapp.com','canalbellavista-7781607eb0d1.herokuapp.com']
 
 
 
