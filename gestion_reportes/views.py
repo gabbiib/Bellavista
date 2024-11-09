e68602a5bbfd45088d0fd1ad6b0c70d0cbb2ec76
antonellaalvarez <150460397+antonellaalvarez@users.noreply.github.com>
2024-11-08 22:53:08 -0300

    Update views.py

--- gestion_reportes/views.py
+++ gestion_reportes/views.py
@@ -73,7 +73,7 @@ def reporte_view(request):
             for admin in administradores:
                 client.messages.create(
                     body=mensaje,
-                    from_= settings.TWILIO_PHONE_NUMBER,
+                    from_= '+1 859 448 7625',
                     to= admin.telefono
                 )
 
